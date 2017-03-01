403afd2f574951166bb3e186b00c98557c26f9aa
Richard Guay <raguay@customct.com>
2017-03-01 12:30:10 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## DeSelect
+
+Plugin for [fman.io](https://fman.io) to remove the selection from the currently selected files and directories.
+
+Install by uploading "DeSelect" to your [data directory](https://fman.io/docs/customizing-fman)`/Plugins`.
+
+### Usage
+
+Select one or more files or directories and press **`<shift>+x`**. The files and directories should be de-selected now.
+
+### Features
+
+ - It simply removes the current selection from the files in the pane.
---
+++ deselect/__init__.py
@@ -0,0 +1,16 @@
+from fman import DirectoryPaneCommand
+from distutils.file_util import copy_file
+from distutils.dir_util import copy_tree
+import os.path
+
+class Deselect(DirectoryPaneCommand):
+    def __call__(self):
+        selected_files = self.pane.get_selected_files()
+        if len(selected_files) >= 1 or (len(selected_files) == 0 and self.get_chosen_files()):
+            if len(selected_files) == 0 and self.get_chosen_files():
+                selected_files.append(self.get_chosen_files()[0])
+            #
+            # Loop through each file/directory selected.
+            #
+            for filedir in selected_files:
+                self.pane.toggle_selection(filedir)
